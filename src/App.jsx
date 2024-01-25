b5d5fa80089540012e3add27daa67d43b9c8b1b0
Hemant Khinchi <99024438+HemantKhinchiProjects@users.noreply.github.com>
2024-01-25 14:20:22 +0000

    style: Remove unused import of Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { CORE_CONCEPTS } from "./data";
-import Header from "./components/Header";
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
 
